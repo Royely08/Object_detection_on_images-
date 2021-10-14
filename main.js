30e8815e1b31586e7e40749cb4cc2d8d83d3fd14
Lakshna S <78756909+Royely08@users.noreply.github.com>
2021-10-13 20:36:14 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function setup(){
 function draw(){
     image(img,0,0,640,420);
     fill("#ff0000");
-    text("Dog",45,75);
+    text("chestnut drawer",45,75);
     noFill();
     stroke("#ff0000");
     rect(30,60,450,350);
