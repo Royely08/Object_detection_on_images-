b1560cec2da4addce602799c967c8d78ec0e0859
Lakshna S <78756909+Royely08@users.noreply.github.com>
2021-10-13 20:33:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+img = "";
+
+function preload(){
+    img = loadImage('example.png');
+}
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+}
+
+function draw(){
+    image(img,0,0,640,420);
+    fill("#ff0000");
+    text("Dog",45,75);
+    noFill();
+    stroke("#ff0000");
+    rect(30,60,450,350);
+}
